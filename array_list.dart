72d73b645a636e4df0954c0c3b65e3e836735fb8
riswanda776 <35683696+riswanda776@users.noreply.github.com>
2021-10-19 08:36:03 +0700

    Update array_list.dart

--- array_list.dart
+++ array_list.dart
@@ -1,4 +1,6 @@
 void main() {
+  
+  // Example 1
   List<String> name = [
     "Joko",
     "Owi"
@@ -7,5 +9,19 @@ void main() {
   name.forEach((nama) => {
     print(nama)
   });
+  // Print all
+  
+  // Example 2
+  
+    List<String> city = [
+    "London",
+    "Manchester",
+    "Montreal",
+    "San Salvador"
+  ];
+  
+  print(city[1]);
+  // Output : Manchester
+
   
 }

797d78b6d52734ba3bbfae338d449a317825c085
riswanda776 <35683696+riswanda776@users.noreply.github.com>
2021-10-19 08:32:46 +0700

    Update penjumlahan.dart

--- penjumlahan.dart
+++ penjumlahan.dart
@@ -4,4 +4,11 @@ void main() {
   
   int count = number1 + number2;
   print("Result : ${count}");
+  
+  //////////// Example 2
+  int number3 = 5;
+  int number4 = 5;
+   int count2 = number3 + number4;
+  print("Result : ${count}");
+  
 }
